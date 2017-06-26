c87c6336eaddd5618964c98e7623d30ae540c57c
Zina Muhamediarova <muhamediarovazs@mail.ru>
2017-06-26 12:01:19 +0600

    first commit

---
+++ readme.txt
@@ -0,0 +1,10 @@
+node 7.4.0
+gulp 3.9.1
+bower 1.8.0
+
+.gitignore
+
+node_modules
+bower_components
+build
+.idea
---
+++ src/js/main.js
@@ -0,0 +1,9 @@
+
+$(document).ready(function() {
+	$('.owl-carousel').owlCarousel({
+		loop:true,
+	    nav:true,
+	    items:1,
+	    dots: true
+	});
+});
